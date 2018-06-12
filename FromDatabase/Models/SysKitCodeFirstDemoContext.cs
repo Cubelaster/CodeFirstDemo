de7c11439684892036e546386ada821b24f3e9cb
Cubelaster <david.cubela@gmail.com>
2018-06-12 21:56:13 +0200

    Recreated model from DB

---
+++ FromDatabase/Models/SysKitCodeFirstDemoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FromDatabase.Models
+{
+    public partial class SysKitCodeFirstDemoContext : DbContext
+    {
+        public SysKitCodeFirstDemoContext()
+        {
+        }
+
+        public SysKitCodeFirstDemoContext(DbContextOptions<SysKitCodeFirstDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Companies> Companies { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SysKitCodeFirstDemo;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasIndex(e => e.CompanyId);
+
+                entity.HasOne(d => d.Company)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.CompanyId);
+            });
+        }
+    }
+}
