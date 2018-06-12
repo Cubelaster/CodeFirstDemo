a4000fe4bc2a637adb77b35f6303e2b15260eee9
Cubelaster <david.cubela@gmail.com>
2018-06-12 22:03:15 +0200

    Initial migration

--- FromDatabase/Migrations/20180612195805_Initial.cs
+++ FromDatabase/Migrations/20180612195805_Initial.cs
@@ -8,57 +8,57 @@ namespace FromDatabase.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Companies",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    ModifiedByEmployeeId = table.Column<int>(nullable: true),
-                    DateCreated = table.Column<DateTime>(nullable: false),
-                    DateModified = table.Column<DateTime>(nullable: true),
-                    Status = table.Column<int>(nullable: false),
-                    Name = table.Column<string>(nullable: true),
-                    VatId = table.Column<string>(nullable: true),
-                    CreatedByEmployeeId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Companies", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Companies",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(nullable: false)
+            //            .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            //        ModifiedByEmployeeId = table.Column<int>(nullable: true),
+            //        DateCreated = table.Column<DateTime>(nullable: false),
+            //        DateModified = table.Column<DateTime>(nullable: true),
+            //        Status = table.Column<int>(nullable: false),
+            //        Name = table.Column<string>(nullable: true),
+            //        VatId = table.Column<string>(nullable: true),
+            //        CreatedByEmployeeId = table.Column<int>(nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Companies", x => x.Id);
+            //    });
 
-            migrationBuilder.CreateTable(
-                name: "Employees",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    ModifiedByEmployeeId = table.Column<int>(nullable: true),
-                    DateCreated = table.Column<DateTime>(nullable: false),
-                    DateModified = table.Column<DateTime>(nullable: true),
-                    Status = table.Column<int>(nullable: false),
-                    FirstName = table.Column<string>(nullable: true),
-                    LastName = table.Column<string>(nullable: true),
-                    DateOfBirth = table.Column<DateTime>(nullable: true),
-                    CompanyId = table.Column<int>(nullable: false),
-                    CreatedByEmployeeId = table.Column<int>(nullable: false),
-                    IsPotentialBuyer = table.Column<bool>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Employees", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Employees_Companies_CompanyId",
-                        column: x => x.CompanyId,
-                        principalTable: "Companies",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Employees",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(nullable: false)
+            //            .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+            //        ModifiedByEmployeeId = table.Column<int>(nullable: true),
+            //        DateCreated = table.Column<DateTime>(nullable: false),
+            //        DateModified = table.Column<DateTime>(nullable: true),
+            //        Status = table.Column<int>(nullable: false),
+            //        FirstName = table.Column<string>(nullable: true),
+            //        LastName = table.Column<string>(nullable: true),
+            //        DateOfBirth = table.Column<DateTime>(nullable: true),
+            //        CompanyId = table.Column<int>(nullable: false),
+            //        CreatedByEmployeeId = table.Column<int>(nullable: false),
+            //        IsPotentialBuyer = table.Column<bool>(nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Employees", x => x.Id);
+            //        table.ForeignKey(
+            //            name: "FK_Employees_Companies_CompanyId",
+            //            column: x => x.CompanyId,
+            //            principalTable: "Companies",
+            //            principalColumn: "Id",
+            //            onDelete: ReferentialAction.Cascade);
+            //    });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Employees_CompanyId",
-                table: "Employees",
-                column: "CompanyId");
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_Employees_CompanyId",
+            //    table: "Employees",
+            //    column: "CompanyId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
